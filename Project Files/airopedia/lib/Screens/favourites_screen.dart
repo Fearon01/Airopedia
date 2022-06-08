4c1c92bd5a4348ffac2a522c7c1dfa67d0aac39b
Joshua Fearon <jfearon00@gmail.com>
2022-06-08 20:57:24 +0100

    Fixed Favourites Screen Class Name

--- Project Files/airopedia/lib/Screens/favourites_screen.dart
+++ Project Files/airopedia/lib/Screens/favourites_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:airopedia/Widgets/navigation_bar.dart';
-class ObservationsScreen extends StatelessWidget 
+class FavouritesScreen extends StatelessWidget 
 {
-  const ObservationsScreen({Key? key}) : super(key: key);
+  const FavouritesScreen({Key? key}) : super(key: key);
   @override
 
   Widget build(BuildContext context) => Scaffold(
