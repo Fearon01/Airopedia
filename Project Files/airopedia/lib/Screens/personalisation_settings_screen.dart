abede69e01ca9c4f36ed5153e519c16ed272d763
Joshua Fearon <jfearon00@gmail.com>
2022-07-27 04:34:28 +0100

    Added Personalisation Screen

---
+++ Project Files/airopedia/lib/Screens/personalisation_settings_screen.dart
@@ -0,0 +1,30 @@
+import 'package:airopedia/Widgets/language_button.dart';
+import 'package:flutter/material.dart';
+
+class PersonalisationSettingsScreen extends StatelessWidget 
+{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: const Color(0xff0d67b5),
+      body: Center(
+        child: ListView (
+        padding: const EdgeInsets.only(top: 100.0),
+        children:  [
+          const Align(
+        alignment: Alignment.topCenter,
+        child: Text(
+        'Personalisation', 
+        style: TextStyle(
+          fontFamily: 'HVD_Comic_Serif_Pro',
+          fontSize: 42,
+          color: Color(0xfff7f3e8),
+            ),
+                      ),
+        ),
+        //LanguageButton(context)
+        ],
+      )
+      )
+  );
+
+}
