292f7dc59f9873af1c014c0afbfdcda6ad341cb6
Joshua Fearon <jfearon00@gmail.com>
2022-07-27 22:25:45 +0100

    Added Transition to Languages Screen

--- Project Files/airopedia/lib/Screens/personalisation_settings_screen.dart
+++ Project Files/airopedia/lib/Screens/personalisation_settings_screen.dart
@@ -21,7 +21,7 @@ class PersonalisationSettingsScreen extends StatelessWidget
             ),
                       ),
         ),
-        //LanguageButton(context)
+        LanguageButton(context)
         ],
       )
       )
